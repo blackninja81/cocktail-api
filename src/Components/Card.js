e8511bb111b3e30d155d53f89ac4c6daf06d6105
blackninja81 <johnkaburu81@gmail.com>
2021-07-27 19:19:44 +0300

    designed front-page

---
+++ src/Components/Card.js
@@ -0,0 +1,28 @@
+
+function Card() {
+    return (
+        <div className="card-body">
+            <div className='search_box'>
+                {/* <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <div class="container-fluid">
+                        <form class="d-flex">
+                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button class="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                    </div>
+                </nav> */}
+            </div>
+            <div className='cocktail_img'>
+
+            </div>
+            <div className='cocktail_ingridients'>
+
+            </div>
+            <div className='cocktail_prep'>
+
+            </div>
+
+        </div>
+    )
+}
+export default Card;
