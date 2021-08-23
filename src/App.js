ffbe1d908416ab9ae291bf41f9415ce15c2786b2
blackninja81 <johnkaburu81@gmail.com>
2021-08-23 16:54:42 +0300

    route update 2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import HomePage from './Pages/HomePage';
 import SearchPage from './Pages/SearchPage';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Route exact path="/" component={HomePage}></Route>
-        <Route path="/search" component={SearchPage}></Route>
+        <Switch>
+          <Route exact path="/" component={HomePage}></Route>
+          <Route path="/search" component={SearchPage}></Route>
+        </Switch>
       </div>
     </BrowserRouter>
   );
